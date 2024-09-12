4744b0aedab928f2d64aaace6cae968ed2804235
fantastico0129 <2425692180@qq.com>
2024-09-12 14:21:06 +0800

    fix

--- js-storefront/hisense-jsstorefront/src/app/spartacus/features/smartedit/smart-edit-feature.module.ts
+++ js-storefront/hisense-jsstorefront/src/app/spartacus/features/smartedit/smart-edit-feature.module.ts
@@ -18,7 +18,7 @@ import { SMART_EDIT_FEATURE, SmartEditConfig, SmartEditRootModule } from "@spart
   provideConfig(<SmartEditConfig>{
     smartEdit: {
       storefrontPreviewRoute: 'cx-preview',
-      allowOrigin: 'jsapps.cdrwhdl6-hisenseho1-d1-public.model-t.cc.commerce.ondemand.com,api.cdrwhdl6-hisenseho1-d1-public.model-t.cc.commerce.ondemand.com',
+      allowOrigin: 'jsapps.cdrwhdl6-hisenseho1-d1-public.model-t.cc.commerce.ondemand.com:80,api.cdrwhdl6-hisenseho1-d1-public.model-t.cc.commerce.ondemand.com',
     },
   })
   ]
